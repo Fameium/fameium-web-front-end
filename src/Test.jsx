275892c11e555129e81f32175a6107494b3e8257
harihkh <diagnal@LAPTOP-5OMMK81I.localdomain>
2020-09-13 09:51:59 +0530

    fix: adding form. not completed.

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
-import { Grid, TextField } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 const Test = () => {
 
     return (
-        <Grid container style={{ padding: '20px', backgroundColor: 'red' }} >
-            <Grid item sm={6} style={{ paddingRight: '20px', backgroundColor: 'blue' }}>
-                <div style={{ height: '50px', width: '50px', backgroundColor: 'black' }}></div>
+        <Grid container style={{backgroundColor: 'yellowgreen'}} spacing={2} >
+            <Grid item sm={6} xs={12}>
+                <div style={{ height: '50px', width: '100%', backgroundColor: 'black' }}></div>
             </Grid>
-            <Grid item sm={6} style={{ paddingLeft: '20px', backgroundColor: 'blue' }}>
-                <div style={{ height: '50px', width: '50px', backgroundColor: 'black' }}></div>
+            <Grid item sm={6} xs={12} >
+                <div style={{ height: '50px', width: '100%', backgroundColor: 'grey' }}></div>
             </Grid>
-            <Grid item container sm={6} style={{ padding: '10px', backgroundColor: 'yellow' }} >
-                <Grid item sm={6}> <div style={{ height: '50px', width: '100%', backgroundColor: 'black' }}></div></Grid>
-                <Grid item sm={6}>  <div style={{ height: '50px', width: '100%', backgroundColor: 'green' }}></div></Grid>
-                <Grid item container sm={12} style={{ backgroundColor: 'white' }}>
-                    <Grid item sm={4} ><div style={{ height: '50px', width: '100%', backgroundColor: 'purple' }}></div></Grid>
-                    <Grid item sm={8} ><div style={{ height: '50px', width: '100%', backgroundColor: 'violet' }}></div></Grid>
-                </Grid>
-                <Grid item container sm={12} style={{ backgroundColor: 'red' }}>
-                    <TextField label="Project Name" variant='outlined' color="secondary" />
 
+            <Grid item container sm={6} xs={12} style={{ backgroundColor: 'orange' }} >
+                <div style={{ height: '50px', width: '100%', backgroundColor: 'black' }}></div>
+                <div style={{ height: '50px', width: '100%', backgroundColor: 'green' }}></div>
+                <Grid item container style={{ backgroundColor: 'red' }} >
+                    <Grid item xs={4} ><div style={{ height: '50px', width: '100%', backgroundColor: 'purple' }}></div></Grid>
+                    <Grid item xs={8} ><div style={{ height: '50px', width: '100%', backgroundColor: 'violet' }}></div></Grid>
                 </Grid>
+                <Grid item container style={{ backgroundColor: 'red' }}>
+                    <Grid item xs={4} ><div style={{ height: '50px', width: '100%', backgroundColor: 'violet' }}></div></Grid>
+                    <Grid item xs={4} ><div style={{ height: '50px', width: '100%', backgroundColor: 'purple' }}></div></Grid>
+                    <Grid item xs={4} ><div style={{ height: '50px', width: '100%', backgroundColor: 'yellow' }}></div></Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item container sm={6} xs={12} style={{ backgroundColor: 'red' }} justify='flex-start' alignItems='flex-start' alignContent='flex-start'  >
+                <Grid item xs={12} sm={6}> <div style={{ height: '50px', width: '100%', backgroundColor: 'green' }}></div></Grid>
+                <Grid item xs={12} sm={6}>  <div style={{ height: '50px', width: '100%', backgroundColor: 'black' }}></div></Grid>
+
+                <Grid item xs={12} ><div style={{ height: '50px', width: '100%', backgroundColor: 'purple' }}></div></Grid>
+
             </Grid>
 
         </Grid>
